8a5ac6b0116d8e68c39708561ba0a99a0425e308
aks <animeshkumarsrivastava13@gmail.com>
2023-07-15 10:23:20 +0530

    This is a counter-step  clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Steps />
     </div>
   );
 }
 
-export default App;
+function Steps() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    step > 1 && setStep((currentState) => currentState - 1);
+  }
+  function handleNext() {
+    setStep((currentState) => currentState + 1);
+  }
+  return (
+    <div className="Container">
+      <div>
+        <button
+          style={{ backgroundColor: "#fff", color: "#000" }}
+          onClick={handleNext}
+        >
+          &#43;
+        </button>
+        <span>Step : {step}</span>
+        <button
+          style={{ backgroundColor: "#fff", color: "#000" }}
+          onClick={handlePrevious}
+        >
+          &#8722;
+        </button>
+        <CounterButton countObj={step} />
+      </div>
+    </div>
+  );
+}
+function CounterButton({ countObj }) {
+  const [count, setCount] = useState(0);
+
+  // Getting the current date and updating it..........(Simple)
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  // This is Bad Practise............
+  // const [date, setDate] = useState([
+  //   new Date().getDay,
+  //   new Date().getMonth,
+  //   new Date().getFullYear,
+  // ]);
+
+  function handlePrevious() {
+    count > 0 && setCount((currentCount) => currentCount - countObj);
+  }
+  function handleNext() {
+    setCount((currentCount) => currentCount + countObj);
+  }
+
+  return (
+    <div className="Container">
+      <div>
+        <button
+          style={{ backgroundColor: "#fff", color: "#000" }}
+          onClick={handleNext}
+        >
+          &#43;
+        </button>
+        <span>Count : {count}</span>
+        <button
+          style={{ backgroundColor: "#fff", color: "#000" }}
+          onClick={handlePrevious}
+        >
+          &#8722;
+        </button>
+      </div>
+      <p>
+        {count} days from today is {date.toDateString()}
+      </p>
+    </div>
+  );
+}
